827ea296d46b61cf1faab2e08216f0a26973475f
SiongSng <rrt467778@gmail.com>
2021-11-19 21:16:03 +0800

    1.18 支援

--- bin/function/RPMTWData.dart
+++ bin/function/RPMTWData.dart
@@ -1,5 +1,10 @@
 class RPMTWData {
-  static List<String> gameVersions = ['1.12', '1.16', '1.17'];
+  static List<String> gameVersions = ['1.12', '1.16', '1.17', '1.18'];
 
-  static Map<String, String> gameVersionRefs = {"1.12": "Translated-1.12", "1.16": "Translated", "1.17": "Translated-1.17"};
+  static Map<String, String> gameVersionRefs = {
+    "1.12": "Translated-1.12",
+    "1.16": "Translated",
+    "1.17": "Translated-1.17",
+    "1.18": "Translated-1.18"
+  };
 }
